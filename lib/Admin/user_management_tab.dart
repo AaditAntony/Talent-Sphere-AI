f1998beb160de2a3b9cb3d4f6568d1dc1a213d27
Aadit Antony <aaditantony1112@gmail.com>
2026-02-15 20:22:24 +0530

    the usermanagemnet is added

---
+++ lib/Admin/user_management_tab.dart
@@ -0,0 +1,68 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class UsersManagementTab extends StatelessWidget {
+  const UsersManagementTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+      stream: FirebaseFirestore.instance
+          .collection('users')
+          .where('role', isEqualTo: 'user')
+          .snapshots(),
+      builder: (context, snapshot) {
+
+        if (!snapshot.hasData) {
+          return const Center(child: CircularProgressIndicator());
+        }
+
+        final users = snapshot.data!.docs;
+
+        if (users.isEmpty) {
+          return const Center(child: Text("No Users Found"));
+        }
+
+        return ListView.builder(
+          padding: const EdgeInsets.all(20),
+          itemCount: users.length,
+          itemBuilder: (context, index) {
+
+            final data = users[index];
+
+            return Card(
+              elevation: 4,
+              margin: const EdgeInsets.only(bottom: 15),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: ListTile(
+                leading: const CircleAvatar(
+                  child: Icon(Icons.person),
+                ),
+                title: Text(
+                  data['name'] ?? "",
+                  style: const TextStyle(
+                      fontWeight: FontWeight.bold),
+                ),
+                subtitle: Text(data['email'] ?? ""),
+                trailing: const Icon(Icons.arrow_forward_ios),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (_) => UserDetailPage(
+                        userId: data.id,
+                        userData: data.data(),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            );
+          },
+        );
+      },
+    );
+  }
+}
