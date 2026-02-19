cdae17c6002f4cf53754bc7c61c1bb81bcc24e53
Aadit Antony <aaditantony1112@gmail.com>
2026-02-19 10:16:05 +0530

    the overview shot is done

---
+++ lib/company/company_overview_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CompanyOverviewPage extends StatelessWidget {
+  const CompanyOverviewPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text(
+        "Overview",
+        style: TextStyle(fontSize: 20),
+      ),
+    );
+  }
+}
