566f55c69a23d9aa1c8d0bf3a35ea3e04b9e08f4
Aadit Antony <aaditantony1112@gmail.com>
2026-03-25 15:43:48 +0530

    the resume section is not added

---
+++ lib/company/pdf_opener_stub.dart
@@ -0,0 +1,5 @@
+// Stub for non-web platforms
+void openPdfInBrowser(String base64Pdf) {
+  // Not supported on this platform
+  throw UnsupportedError('Cannot open PDF in browser on this platform');
+}
